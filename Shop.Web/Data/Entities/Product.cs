c899a21d0716fbf7cf70d68af7e31ca52df79aea
Jeisson damian becerra rodriguez <jeisson_becerra45@hotmail.com>
2019-11-10 23:08:48 -0500

    OrderBy Abecedary

--- Shop.Web/Data/Entities/Product.cs
+++ Shop.Web/Data/Entities/Product.cs
@@ -31,6 +31,19 @@
 
         public User User { get; set; }
 
+        public string ImageFullPath
+        {
+            get
+            {
+                if(string.IsNullOrEmpty(this.ImageUrl))
+                {
+                    return null;
+                }
+
+                return $"https://shopproyecto.azurewebsites.net{this.ImageUrl.Substring(1)}";
+            } 
+        }
+
     }
 
 }
