640c39ba2c430cf523b4f421be2dc6d9f7f9bdbf
Jeisson damian becerra rodriguez <jeisson_becerra45@hotmail.com>
2019-11-13 18:40:35 -0500

    Date Picker

---
+++ Shop.Web/Models/DeliverViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop.Web.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class DeliverViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Delivery date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime DeliveryDate { get; set; }
+    }
+}
