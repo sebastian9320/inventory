0786792d85139e7d4aa672386c6e0d57c86f2925
Jeisson damian becerra rodriguez <jeisson_becerra45@hotmail.com>
2019-11-15 14:53:12 -0500

    Shop Proyect Complete for App

--- Shop.UIForms/Shop.UIForms.Android/MainActivity.cs
+++ Shop.UIForms/Shop.UIForms.Android/MainActivity.cs
@@ -9,7 +9,11 @@ using Android.OS;
 
 namespace Shop.UIForms.Droid
 {
-    [Activity(Label = "Shop.UIForms", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Shop.UIForms", 
+        Icon = "@mipmap/ic_launcher", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = false, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
